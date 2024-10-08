19308fe01ca9adfee0a448e3b65d2f6205dad059
Waynolt <Waynolt@users.noreply.github.com>
2024-10-09 01:41:57 +0200

    updated snappymenus

--- Mods/SWM - SnappyMenus.rb
+++ Mods/SWM - SnappyMenus.rb
@@ -1,26 +1,29 @@
-def pbSetSpritesToColor(sprites,color)
-  return if !sprites||!color
-  colors={}
+def pbSetSpritesToColor(sprites, color)
+  return if !sprites || !color
+
+  colors = {}
   for i in sprites
     next if !i[1] || pbDisposed?(i[1])
-    colors[i[0]]=i[1].color.clone
-    i[1].color=pbSrcOver(i[1].color,color)
+
+    colors[i[0]] = i[1].color.clone
+    i[1].color = pbSrcOver(i[1].color, color)
   end
   #####MODDED, was Graphics.update
   #####MODDED, was Input.update
   for i in colors
     next if !sprites[i[0]]
-    sprites[i[0]].color=i[1]
+
+    sprites[i[0]].color = i[1]
   end
 end
 
 def pbFadeOutIn(z)
-  col=Color.new(0,0,0,0)
-  viewport=Viewport.new(0,0,Graphics.width,Graphics.height)
-  viewport.z=z
+  col = Color.new(0, 0, 0, 0)
+  viewport = Viewport.new(0, 0, Graphics.width, Graphics.height)
+  viewport.z = z
   for j in 0..17
-    col.set(0,0,0,j*15)
-    viewport.color=col
+    col.set(0, 0, 0, j * 15)
+    viewport.color = col
     #####MODDED, was Graphics.update
     #####MODDED, was Input.update
   end
@@ -32,8 +35,8 @@ def pbFadeOutIn(z)
   ensure
     pbPopFade
     for j in 0..17
-      col.set(0,0,0,(17-j)*15)
-      viewport.color=col
+      col.set(0, 0, 0, (17 - j) * 15)
+      viewport.color = col
       #####MODDED, was Graphics.update
       #####MODDED, was Input.update
     end
